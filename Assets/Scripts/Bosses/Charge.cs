37850b73e7cb4f100255a5895ca5fc73deab018d
Agustin Moure <amoure@ucm.es>
2019-03-29 16:56:26 +0100

    Creada la rama Wolf

---
+++ Assets/Scripts/Bosses/Charge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charge : MonoBehaviour {
+    public int speed;
+    Rigidbody2D rigidbody2D;
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = transform.parent.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
