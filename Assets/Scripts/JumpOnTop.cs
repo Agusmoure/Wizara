be0c50e718efceb04860b00ca45417037789620d
Agustin Moure <amoure@ucm.es>
2019-04-03 10:23:46 +0200

    Modificado  RatPrefab y JumpOnTop

--- Assets/Scripts/JumpOnTop.cs
+++ Assets/Scripts/JumpOnTop.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpOnTop : MonoBehaviour {
-    
     //Si el jugador colisiona con la parte superior de la rata la aplasta, matandola y rebotando
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -15,8 +14,8 @@ public class JumpOnTop : MonoBehaviour {
         //La variable "angle" es el angulo entre el vector "right" y la normal del punto de contacto
         float angle = Vector2.Angle((Vector2)transform.right, contacts[0].normal);
 
-        //Si el objeto con el que se ha colisionado es el jugador y "angle" es practicamente 0 sabemos que la colisión ha sido por arriba.
-         if (collision.gameObject.tag == "Player" && angle < 0.01&&transform.position.y<=collision.transform.position.y)
+        //Si el objeto con el que se ha colisionado es el jugador y "angle" es practicamente 0 sabemos que la colisión ha sido por arriba además se comprueba que la posicion del jugador mas1/4 de su escala para que si salta algun frame no de fallos(como pasa con 1/2 de la escala) estan por encima de la posicion del objeto.
+         if (collision.gameObject.tag == "Player" && angle < 0.01&&transform.position.y<=(collision.transform.position.y-collision.transform.localScale.y/4))
          {
             //Obtenemos el script "Life" de la rata y restamos a su vida la misma cantidad que tenga actualmente, haciendo que esta caiga a 0
             Life ratLife = transform.parent.GetComponent<Life>();
