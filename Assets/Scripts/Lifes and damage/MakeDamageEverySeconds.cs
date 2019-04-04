f1589b217cda7f756548d1174ee58fc5850a7101
Agustin Moure <amoure@ucm.es>
2019-04-04 21:02:53 +0200

    Creadas la paleta de acido y el script MakeDamageEverySeconds

---
+++ Assets/Scripts/Lifes and damage/MakeDamageEverySeconds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeDamageEverySeconds : MonoBehaviour {
+
+    public int damage;
+    Collider2D collider;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("esqueletinho");
+        GetCollider2D(collision);
+        Invoke("MakeDamage", 2);
+
+    }
+    void GetCollider2D(Collider2D collision)
+    {
+        collider = collision;
+    }
+    void MakeDamage()
+    {
+        if (collider.GetComponent<Life>() != null && !GameManager.instance.GetInvulnerablePlayer()) collider.GetComponent<Life>().LoseLife(damage);
+    }
+}
