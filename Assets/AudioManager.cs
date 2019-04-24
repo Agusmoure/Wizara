3dba90aa7f0ffef8447d0f26bbf45c51dd03fcee
Will7461 <43720307+Will7461@users.noreply.github.com>
2019-04-24 20:28:37 +0200

    Primera version de AudioManager

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -10,11 +10,13 @@ public class AudioManager : MonoBehaviour {
     [System.Serializable]
     public struct Sound
     {
+        // Las siguientes 3 variables las guardarán cada componente del array de structs.
         public string name;
         public AudioClip clip;
         [Range(0,1)]
         public float volume;
         [HideInInspector]
+        // A las variables de tipo AudioSource se les puede asignar un clip y un volumen entre otros valores (tienen su propio .clip y .volume que en este caso estará vacío hasta más adelante).
         public AudioSource source;
     }
     private void Awake()
@@ -28,11 +30,22 @@ public class AudioManager : MonoBehaviour {
         {
             Destroy(this.gameObject);
         }
+
+        for(int i = 0; i < sound.Length; i++)
+        {
+            // Al iniciar se crean todos los nuevos componentes AudioSource según el número de componentes del array creado.
+            sound[i].source = gameObject.AddComponent<AudioSource>();
+            // A cada componente source de tipo AudioSource se le asigna el clip y el volumen guardado en el array.
+            sound[i].source.clip = sound[i].clip;
+            sound[i].source.volume = sound[i].volume;
+        }
     }
     // Use this for initialization
     void Start () {
-        theme.Play();
-	}
+        // Comunica al GM de quien es el AudioManager.
+        GameManager.instance.ThisAudioManager(this);
+        // theme.Play();
+    }
 	
 	public void PlayAudio(string name)
     {
@@ -43,9 +56,11 @@ public class AudioManager : MonoBehaviour {
         }
         try
         {
-            sound[i].source = GetComponent<AudioSource>();
-            sound[i].source.clip = sound[i].clip;
-            sound[i].source.volume = sound[i].volume;
+            sound[i].source.Play();
+        }
+        catch
+        {
+            Debug.LogWarning("No existe el audio con nombre "+name+" que se intenta reproducir.");
         }
     }
 }
