b9a29cba2d49d838fa3389ba68437f971fe90c8b
Agustin Moure <amoure@ucm.es>
2019-04-28 22:12:45 +0200

    Eliminado código inservible, divididos los casos no tilemap y tilemap(este sigue sin funcionar) y cambiado de on stay a enter

--- Assets/OnCollissionInstance.cs
+++ Assets/OnCollissionInstance.cs
@@ -1,28 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
-public class Instance : MonoBehaviour {
+public class Instance : MonoBehaviour
+{
     public GameObject gameObjectToInstance;
-    public void InstanceThis(GameObject collision) {
+    public void InstanceNoTileMap(GameObject collision)
+    {
         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
         BoxCollider2D collider2D = collision.GetComponent<BoxCollider2D>();
         Vector3 position;
         if (collider2D != null)
-            position = collider2D.gameObject.transform.position-Vector3.right*Mathf.Abs(collider2D.offset.x)-Vector3.up* Mathf.Abs(collider2D.offset.y) + Vector3.up * (collider2D.size.y / 2);
-        else
-        {
-            if (collider2D = null)
-            {
-                CompositeCollider2D compositeCollider2D = collision.GetComponent<CompositeCollider2D>();
-                position = new Vector3(0, 0, 0);
-            }
-            else position = new Vector3(0, 0, 0);
-        }
-
+            position = collider2D.gameObject.transform.position - Vector3.right * Mathf.Abs(collider2D.offset.x) - Vector3.up * Mathf.Abs(collider2D.offset.y) + Vector3.up * (collider2D.size.y / 2);
+        else position = new Vector3(0, 0, 0);
         GameObject newGOgbject = Instantiate(gameObjectToInstance, position, Quaternion.identity, null);
         newGOgbject.transform.Rotate(Vector3.forward, 90);
-        //collider2D.size.y
+    }
+    public void InstanceTileMap( ContactPoint2D contact, Tilemap tile)
+    {
+        Vector3 position;
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        position = contact.point + Vector2.up * (tile.tileAnchor.y / 2);
+        GameObject newGOgbject = Instantiate(gameObjectToInstance, position, Quaternion.identity, null);
+        newGOgbject.transform.Rotate(Vector3.forward, 90);
     }
 }
--- Assets/Scripts/AumentHeightEveryXseconds.cs
+++ Assets/Scripts/AumentHeightEveryXseconds.cs
@@ -1,44 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class AumentHeightEveryXseconds : MonoBehaviour {
+public class AumentHeightEveryXseconds : MonoBehaviour
+{
     public float seconds, aument;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         InvokeRepeating("Aument", 0, seconds);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        Instance inst = GetComponent<Instance>();
-        if (inst != null)
-        {
-            inst.InstanceThis(collision.gameObject);
-            Destroy(gameObject);
-        }
-        else
-            Destroy(gameObject);
     }
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Instance inst = GetComponent<Instance>();
         if (inst != null)
         {
-            inst.InstanceThis(collision.gameObject);
+            TilemapCollider2D tilemapCollider = collision.gameObject.GetComponent<TilemapCollider2D>();
+            Tilemap tile = null;
+            ContactPoint2D[] contacts = null;
+            if (tilemapCollider != null)
+            {
+                tile = tilemapCollider.gameObject.GetComponent<Tilemap>();
+                contacts = new ContactPoint2D[1];
+                tilemapCollider.GetContacts(contacts);
+                Debug.Log(contacts[0].point);
+                inst.InstanceTileMap(contacts[0], tile);
+            }
+            else
+            inst.InstanceNoTileMap(collision.gameObject);
             Destroy(gameObject);
         }
         else
-        Destroy(gameObject);
+            Destroy(gameObject);
     }
     void Aument()
     {
-    
-        SpriteRenderer sprite = GetComponent < SpriteRenderer > ();
+
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
         float aumented = sprite.size.y + aument;
         sprite.size = new Vector2(sprite.size.x, aumented);
         transform.position = transform.position + Vector3.down * aument / 2;
