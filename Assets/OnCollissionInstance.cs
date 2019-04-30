c8814e949df1dd95d4f10d05f1091c7729358014
Agustin Moure <amoure@ucm.es>
2019-04-30 21:01:22 +0200

    EL RAYO YA CREA LA ZONA BIEEEEN ^^

--- Assets/OnCollissionInstance.cs
+++ Assets/OnCollissionInstance.cs
@@ -1,33 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class Instance : MonoBehaviour
 {
     public GameObject gameObjectToInstance;
-    public void InstanceNoTileMap(GameObject collision)
+    public void Instantiate(Vector2 vector)
     {
-        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-        CompositeCollider2D compositeCollider2D = collision.GetComponent<CompositeCollider2D>();
-        Vector3 position;
-        if (compositeCollider2D != null)
-        {
-            //position = compositeCollider2D.gameObject.transform.position - Vector3.right * Mathf.Abs(compositeCollider2D.offset.x) - Vector3.up * Mathf.Abs(compositeCollider2D.offset.y) + Vector3.up * (obtener tamaño / 2);
-        }
-        BoxCollider2D collider2D = collision.GetComponent<BoxCollider2D>();
-        if (collider2D != null)
-            position = collider2D.gameObject.transform.position - Vector3.right * Mathf.Abs(collider2D.offset.x) - Vector3.up * Mathf.Abs(collider2D.offset.y) + Vector3.up * (collider2D.size.y / 2);
-        else position = new Vector3(0, 0, 0);
-        GameObject newGOgbject = Instantiate(gameObjectToInstance, position, Quaternion.identity, null);
-        newGOgbject.transform.Rotate(Vector3.forward, 90);
-    }
-    public void InstanceTileMap( ContactPoint2D contact, Tilemap tile)
-    {
-        //Tras recibir el punto de contacto inicializamos ahí el objeto y lo rotamos 90º en el eje z
-        Vector3 position;
-        position = contact.point /*+ Vector2.up * (tile.tileAnchor.y / 2) no sé si deberiamos sumarle el ancho del tilemap*/;
-        GameObject newGOgbject = Instantiate(gameObjectToInstance, position, Quaternion.identity, null);
+        GameObject newGOgbject = Instantiate(gameObjectToInstance, vector, Quaternion.identity, null);
         newGOgbject.transform.Rotate(Vector3.forward, 90);
     }
 }
+
--- Assets/Scripts/AumentHeightEveryXseconds.cs
+++ Assets/Scripts/AumentHeightEveryXseconds.cs
@@ -11,33 +11,25 @@ public class AumentHeightEveryXseconds : MonoBehaviour
     {
         InvokeRepeating("Aument", 0, seconds);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         Instance inst = GetComponent<Instance>();
         if (inst != null)
         {
-            //Guardamos el tilemap collider del objeto con el que collisionamos
-            TilemapCollider2D tilemapCollider = collision.gameObject.GetComponent<TilemapCollider2D>();
-            //inicializamos el tilemap y los puntos de contacto
-            Tilemap tile = null;
-            ContactPoint2D[] contacts = new ContactPoint2D[1];
-            //si tiene tilemap collider
-            if (tilemapCollider != null)
+            foreach (ContactPoint2D contact in collision.contacts)
             {
-                //obtenemos el tilemap y los puntos de contacto
-                tile = tilemapCollider.gameObject.GetComponent<Tilemap>();
-                contacts = new ContactPoint2D[1];
-                tilemapCollider.GetContacts(contacts);
-               //llamamos al metodo que instacia el objeto si es en un tilemap
-                inst.InstanceTileMap(contacts[0], tile);
+                Vector2 hitPoint = contact.point;
+                //Instantiate(explosion, new Vector3(hitPoint.x, hitPoint.y, 0), Quaternion.identity);
+                inst.Instantiate(hitPoint);
             }
-            //si no tiene tilemap realiza el metodo de no tilemap
-            else
-            inst.InstanceNoTileMap(collision.gameObject);
             Destroy(gameObject);
         }
         else
             Destroy(gameObject);
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("C" + hit.point);
     }
     void Aument()
     {
