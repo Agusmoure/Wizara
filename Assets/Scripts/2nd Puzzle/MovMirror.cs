7d5e0e86eee0a373253dbc522f11a80da63a3e32
PabloCubells <pablocub@ucm.es>
2019-05-10 13:00:20 +0200

    FUNCIONA!!!!
    Se pueden mover y girar.
    Falta finalizar y cambiar color al fichar un espejo.

--- Assets/Scripts/2nd Puzzle/MovMirror.cs
+++ Assets/Scripts/2nd Puzzle/MovMirror.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class MovMirror : MonoBehaviour
 {
     public Material select, defau;
-    //public GameObject[] mirrors = new GameObject[3];
-    public string letter;
     MeshRenderer mesh;
     bool selected;
 
@@ -21,15 +19,16 @@ public class MovMirror : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.get)
-        {
-            Debug.Log("Click");
-            selected = !selected;
-            //mesh.material = select;
-            if (!selected)
-                mesh.material = select;
-            else if (selected)
-                mesh.material = defau;
-        }
+
 	}
+
+    //Cambia el material a seleccionado o no seleccionado.
+    public void changeColor()
+    {
+        selected = !selected;
+        if (!selected)
+            mesh.material = select;
+        else if (selected)
+            mesh.material = defau;
+    }
 }
---
+++ Assets/Scripts/2nd Puzzle/SelectMirror.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectMirror : MonoBehaviour
+{
+    const int NUM_MIRRORS = 3;
+    int index, aux;
+    public GameObject[] go = new GameObject[NUM_MIRRORS];
+    public KeyCode keyRight, keyLeft;
+	// Use this for initialization
+	void Start ()
+    {
+        index = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RotationInput(Input.GetAxis("Vertical"));
+        if (Input.GetKeyUp(keyRight) && (index + 1) < go.Length) //right
+        {
+            index++;
+            go[index].GetComponent<MovMirror>().changeColor();
+        }
+        else if (Input.GetKeyUp(keyLeft) && (index - 1) >= 0) //left
+        {
+            index--;
+            go[index].GetComponent<MovMirror>().changeColor();
+        }
+    }
+
+    void RotationInput(float axis)
+    {
+        go[index].transform.Rotate(0, 0, axis);
+    }
+}
