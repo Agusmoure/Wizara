cddd56c48d91f3e1b8bebb733640debfd717ac96
Will7461 <43720307+Will7461@users.noreply.github.com>
2019-05-15 20:16:45 +0200

    Arreglado bug de las puertas

--- Assets/Scripts/KeyDoor.cs
+++ Assets/Scripts/KeyDoor.cs
@@ -9,22 +9,26 @@ public class KeyDoor : MonoBehaviour {
     [TextArea(3, 10)]
     public string[] sentences;
 
-    
+    Collider2D coll;
     public void Unlock()
     {
         locked = false;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Si el método Unlock ha sido llamado por la llave y se cambió el booleano locked, la puerta se podrá abrir en colisión.
-        if (!locked)
+        coll = collision.collider;
+        if(coll == coll.GetComponent<PolygonCollider2D>())
         {
-            Destroy(gameObject);
-        }
-        //si está cerrada, indica al jugador mediante un cuadro de texto que necesita la llave para pasar por esa puerta.
-        else
-        {
-            GameManager.instance.ReturnUIManager().EnableDialogueBox(dialogueName, sentences);
+            //Si el método Unlock ha sido llamado por la llave y se cambió el booleano locked, la puerta se podrá abrir en colisión.
+            if (!locked)
+            {
+                Destroy(gameObject);
+            }
+            //si está cerrada, indica al jugador mediante un cuadro de texto que necesita la llave para pasar por esa puerta.
+            else
+            {
+                GameManager.instance.ReturnUIManager().EnableDialogueBox(dialogueName, sentences);
+            }
         }
     }
 }
